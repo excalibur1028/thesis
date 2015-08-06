010079e7a2ab8a891b71dada5310ba755c83a875
LoveKoBebz <lovekobebz@gmail.com>
2015-08-07 06:17:54 +0800

    Add Application Home Page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    /**
+     * Application home page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function home()
+    {
+        return view('pages.index');
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+@section('content')
+    <div class="row">
+        <div class="container">
+            <h1>Homepage</h1>
+        </div>
+    </div>
+@endsection
