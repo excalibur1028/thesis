2782978259fd551310ce7b43652d986530de4e3a
LoveKoBebz <lovekobebz@gmail.com>
2015-08-09 15:46:12 +0800

    Refactor Application About Page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,78 @@
+<div id="about" class="spied">
+    <div class="row pvxl">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4">
+                    <h2 class="no-margin">Our History</h2>
+                    <p class="text-justify mtm">
+                        Started its humble beginning through the effort of the late Rolando Carlos Sr. (founder).
+                        Using Rolando's expertise in interior designing, he created a family-owned business in 1975 under a different company name,
+                        QuaSer (Quality Service) providing expertise in window treatment and upholstery in the Philippines. In 1988, his wife, Gloria Carlos,
+                        took over the family business which turned into the company name Glo's Decor and opened a small shop in Manila, Philippines.
+                        <br><br>
+                        Today, under a new name, Excellent Home Decor (EHD), the legacy of quality service still runs in the family business.
+                        EHD is under the professional leadership of Rolando's children and grandsons.  Over the years, EHD is still developing its skills and
+                        knowledge in the field of interior designing from window treatment, to  upholstery, to furniture, to design development, to installation
+                        and motorize system. EHD provides outstanding window treatment products and excellent service to our clients.
+                    </p>
+                </div>
+                <div class="col-xs-8">
+                    <img class="img-thumbnail img-responsive" src="{{ asset('img/img-about.jpg') }}" alt="About us">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="showroom" class="spied">
+    <div class="row bg-white pvxl">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-8">
+                    <img class="img-thumbnail img-responsive" src="{{ asset('img/img-about.jpg') }}" alt="About us">
+                </div>
+                <div class="col-xs-4">
+                    <h2 class="no-margin">Show Room</h2>
+                    <p class="text-justify mtm">
+                        We provide our clients something unique and excellent customer service.
+                        All of our custom-made window treatments are on display for our clients to see, feel and operate before they place an order.
+                        Get an expert advice from our creative and talented designer team.
+                    </p>
+                    <address>
+                        <strong>ADDRESS: <br></strong>
+                        210 Tandang Sora Ave,<br>
+                        Brgy. Tandang Sora, Quezon City, Philippines<br><br>
+
+                        <strong>HOUR: <br></strong>
+                        Mon-Sat 9:00AM - 7:00PM<br>
+                        Sun - Appointment Only
+                    </address>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="workforce" class="spied">
+    <div class="row pvxl">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4">
+                    <h2 class="no-margin">Work Force</h2>
+                    <p class="text-justify mtm">
+                        Excellent Home Decor has its own production workshop which divided into to two areas - EHDC is for curtains, drapery & upholstery and
+                        EHDP is for blinds & shades - under its own management delivering high quality custom made curtains, blinds, shades and other interior products.
+                        Excellent Home Decor is partnered with local and international suppliers providing top window treatment materials for preeminent window fashion brands.
+                        <br><br>
+                        EHD Team manages its own inventories and oversees every aspect of the production process.
+                        All products are proudly designed and assembled in the Philippines by Excellent Home Decor Team.
+                        These give our clients lower cost and high quality custom-made window treatment solution.
+                    </p>
+                </div>
+                <div class="col-xs-8">
+                    <img class="img-thumbnail img-responsive" src="{{ asset('img/img-about.jpg') }}" alt="About us">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/scrollspy.blade.php
@@ -0,0 +1,17 @@
+@section('scrollspy')
+    <li id="scrollspy" class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Corporate Info</a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Blog</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#showroom">Showroom</a></li>
+            <li><a href="#workforce">Work Force</a></li>
+            <li><a href="#">Designer Team</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Photo Gallery</a></li>
+            <li><a href="#">Video Gallery</a></li>
+        </ul>
+    </li>
+@endsection
